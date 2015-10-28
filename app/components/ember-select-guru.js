2d75953f6ef7845b68b3c299ecad8f3c090210ec
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-10-28 10:52:58 +0100

    Publishing to github pages

--- app/components/ember-select-guru.js
+++
@@ -1,6 +0,0 @@
-import intersection from 'npm:lodash/array/intersection';
-import difference from 'npm:lodash/array/difference';
-
-import EmberSelectGuru from 'ember-select-guru/components/ember-select-guru';
-
-export default EmberSelectGuru;
